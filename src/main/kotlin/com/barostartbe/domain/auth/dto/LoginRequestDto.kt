d924ffd6e723d63f0d595b6c7f72e34d8c406f35
JaeYunChung <wjdwodbs2334@gmail.com>
2026-02-05 22:07:16 +0900

    fix: notBlank 어노테이션 삭제

--- src/main/kotlin/com/barostartbe/domain/auth/dto/LoginRequestDto.kt
+++ src/main/kotlin/com/barostartbe/domain/auth/dto/LoginRequestDto.kt
@@ -8,11 +8,9 @@ import jakarta.validation.constraints.NotBlank
 data class LoginRequestDto(
     @Schema(description = "유저 로그인 id", example = "baro1234")
     @Max(value = 20)
-    @NotBlank
     val loginId: String?,
 
     @Schema(description = "유저 패스워드", example = "1234")
     @Max(value = 50)
-    @NotBlank
     val password: String?
 )
