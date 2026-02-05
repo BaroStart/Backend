341e0e929aed5c156339325deb894b2c2958ccfc
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2026-02-06 00:42:34 +0900

    feat: 할일 관련 DTO 추가

---
+++ src/main/kotlin/com/barostartbe/domain/todo/dto/request/ChangeToDoStatusReq.kt
@@ -0,0 +1,22 @@
+package com.barostartbe.domain.todo.dto.request
+
+import com.barostartbe.domain.todo.dto.base.TimeSlot
+import com.barostartbe.domain.todo.entity.enums.Status
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotBlank
+
+@Schema(description = "할 일 수정 요청 DTO")
+data class ChangeToDoStatusReq(
+
+    @Schema(description = "할 일 ID", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
+    @NotBlank(message = "id는 필수입니다")
+    val id: Long,
+
+    @Schema(description = "할 일 상태", example = "COMPLETED", requiredMode = Schema.RequiredMode.REQUIRED)
+    @NotBlank(message = "status는 필수입니다")
+    val status: Status,
+
+    @Schema(description = "시간 목록", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    val timeList: List<TimeSlot>?
+) {
+}
