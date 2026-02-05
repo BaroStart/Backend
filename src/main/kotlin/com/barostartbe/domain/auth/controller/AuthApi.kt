64149183337e7061bc35459b230f48e6d16f2723
JaeYunChung <wjdwodbs2334@gmail.com>
2026-02-05 10:07:02 +0900

    feat: swagger 작성

---
+++ src/main/kotlin/com/barostartbe/domain/auth/controller/AuthApi.kt
@@ -0,0 +1,24 @@
+package com.barostartbe.domain.auth.controller
+
+import com.barostartbe.domain.auth.dto.SignupRequestDto
+import com.barostartbe.domain.auth.dto.TokenPairResponseDto
+import com.barostartbe.global.response.ApiResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Tag(name = "Auth API", description = "로그인, 로그아웃 관리 api")
+@RequestMapping("/api/v1")
+interface AuthApi {
+    @PostMapping("/login")
+    @Operation(summary = "로그인 api", description = "로그인에 성공하면 access/refresh 토큰을 반환한다.")
+    fun login(request: HttpServletRequest): ResponseEntity<ApiResponse<TokenPairResponseDto>>
+
+    @PostMapping("/signup")
+    @Operation(summary = "회원가입 api", description = "회원 정보를 db에 저장한다.")
+    fun signup(@RequestBody request: SignupRequestDto): ResponseEntity<ApiResponse<String>>
+}
--- src/main/kotlin/com/barostartbe/domain/auth/controller/AuthController.kt
+++ src/main/kotlin/com/barostartbe/domain/auth/controller/AuthController.kt
@@ -8,22 +8,18 @@ import com.barostartbe.global.response.ApiResponse
 import com.barostartbe.global.response.type.SuccessCode
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1")
 class AuthController(
     val authCommandUseCase: AuthCommandUseCase,
     val authQueryUseCase: AuthQueryUseCase
-) {
-    @PostMapping("/login")
-    fun login(request: HttpServletRequest): ResponseEntity<ApiResponse<TokenPairResponseDto>> = ApiResponse.success(SuccessCode.REQUEST_OK, authQueryUseCase.login(request))
+) : AuthApi {
 
-    @PostMapping("/signup")
-    fun signup(@RequestBody request: SignupRequestDto): ResponseEntity<ApiResponse<String>> {
+    override fun login(request: HttpServletRequest): ResponseEntity<ApiResponse<TokenPairResponseDto>> = ApiResponse.success(SuccessCode.REQUEST_OK, authQueryUseCase.login(request))
+
+    override fun signup(@RequestBody request: SignupRequestDto): ResponseEntity<ApiResponse<String>> {
         authCommandUseCase.createUser(request)
         return ApiResponse.success(SuccessCode.CREATE_OK, "회원가입 되었습니다.")
     }
