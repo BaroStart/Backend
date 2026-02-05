2d10addd39956f33882a861e4f94d03495d4a61c
JaeYunChung <wjdwodbs2334@gmail.com>
2026-02-05 22:01:14 +0900

    comment, subcomment 엔티티 생성

---
+++ src/main/kotlin/com/barostartbe/domain/comment/entity/Comment.kt
@@ -0,0 +1,19 @@
+package com.barostartbe.domain.comment.entity
+
+import com.barostartbe.domain.mentee.entity.Mentee
+import com.barostartbe.global.common.entity.BaseEntity
+import jakarta.persistence.Entity
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "comments")
+class Comment (
+    @ManyToOne
+    @JoinColumn(name = "mentee_id")
+    var mentee: Mentee? = null,
+
+    var comment: String? = null
+): BaseEntity(){
+}
---
+++ src/main/kotlin/com/barostartbe/domain/comment/entity/SubComment.kt
@@ -0,0 +1,24 @@
+package com.barostartbe.domain.comment.entity
+
+import com.barostartbe.domain.user.entity.User
+import com.barostartbe.global.common.entity.BaseEntity
+import jakarta.persistence.Entity
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "sub_comments")
+class SubComment(
+    @ManyToOne
+    @JoinColumn(name = "comment_id")
+    val comment: Comment,
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    val user: User,
+
+    var content: String? = null
+): BaseEntity() {
+
+}
