2a6e43eacd514b593ea60e5b238ab4d93cc080dd
JaeYunChung <wjdwodbs2334@gmail.com>
2026-02-06 18:44:05 +0900

    feat: 레포에 함수 추가

--- src/main/kotlin/com/barostartbe/domain/comment/repository/CommentRepository.kt
+++ src/main/kotlin/com/barostartbe/domain/comment/repository/CommentRepository.kt
@@ -1,7 +1,9 @@
 package com.barostartbe.domain.comment.repository
 
 import com.barostartbe.domain.comment.entity.Comment
+import com.barostartbe.domain.mentee.entity.Mentee
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CommentRepository: JpaRepository<Comment, Long> {
+    fun findAllByMenteeIn(mentee: List<Mentee>): List<Comment>
 }
--- src/main/kotlin/com/barostartbe/domain/comment/repository/SubCommentRepository.kt
+++ src/main/kotlin/com/barostartbe/domain/comment/repository/SubCommentRepository.kt
@@ -1,7 +1,10 @@
 package com.barostartbe.domain.comment.repository
 
+import com.barostartbe.domain.comment.entity.Comment
 import com.barostartbe.domain.comment.entity.SubComment
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface SubCommentRepository: JpaRepository<SubComment, Long> {
+fun findAllByComment(comment: Comment): List<SubComment>
+    fun deleteAllByComment(comment: Comment)
 }
