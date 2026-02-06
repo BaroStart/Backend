92f0b5d6839de54abe917b58d5a038e27c6c2902
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2026-02-06 16:14:10 +0900

    HOTFIX: redis 연결 불가 문제 해결

--- src/main/kotlin/com/barostartbe/global/config/RedisConfig.kt
+++ src/main/kotlin/com/barostartbe/global/config/RedisConfig.kt
@@ -3,7 +3,6 @@ package com.barostartbe.global.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer
@@ -14,12 +13,9 @@ class RedisConfig(
     val objectMapper: JsonMapper
 ) {
     @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory = LettuceConnectionFactory()
-
-    @Bean
-    fun redisTemplate() : RedisTemplate<String, Any>{
+    fun redisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
         return RedisTemplate<String, Any>().apply {
-            connectionFactory = redisConnectionFactory()
+            this.connectionFactory = connectionFactory
             keySerializer = StringRedisSerializer()
             valueSerializer = GenericJacksonJsonRedisSerializer(objectMapper)
         }
