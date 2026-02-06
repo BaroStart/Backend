e166374cb2736b73e077b8f521480c1bdd17e545
JaeYunChung <wjdwodbs2334@gmail.com>
2026-02-06 18:44:56 +0900

    feat: comment, subcomment 조회 함수 생성

--- src/main/kotlin/com/barostartbe/domain/comment/usecase/CommentQueryUseCase.kt
+++ src/main/kotlin/com/barostartbe/domain/comment/usecase/CommentQueryUseCase.kt
@@ -1,7 +1,12 @@
 package com.barostartbe.domain.comment.usecase
 
-import com.barostartbe.domain.comment.entity.Comment
+import com.barostartbe.domain.admin.entity.MentorMenteeMapping
+import com.barostartbe.domain.admin.repository.MentorMenteeMappingRepository
+import com.barostartbe.domain.comment.dto.GetCommentResponseDto
+import com.barostartbe.domain.comment.dto.GetSubCommentResponseDto
 import com.barostartbe.domain.comment.repository.CommentRepository
+import com.barostartbe.domain.comment.repository.SubCommentRepository
+import com.barostartbe.domain.mentor.repository.MentorRepository
 import com.barostartbe.global.annotation.QueryUseCase
 import com.barostartbe.global.error.exception.ServiceException
 import com.barostartbe.global.response.type.ErrorCode
@@ -9,10 +14,26 @@ import org.springframework.data.repository.findByIdOrNull
 
 @QueryUseCase
 class CommentQueryUseCase(
-    private val commentRepository: CommentRepository
+    private val mentorMenteeMappingRepository: MentorMenteeMappingRepository,
+    private val commentRepository: CommentRepository,
+    private val mentorRepository: MentorRepository,
+    private val subCommentRepository: SubCommentRepository
 ) {
-    fun getComment(commentId: Long): Comment {
-        return commentRepository.findByIdOrNull(commentId) ?: throw ServiceException(ErrorCode.NOT_FOUND)
+    fun getAllCommentsForMentor(mentorId: Long): List<GetCommentResponseDto> {
+        val mentor = mentorRepository.findByIdOrNull(mentorId) ?: throw ServiceException(ErrorCode.USER_NOT_FOUND)
+        val mentee = mentorMenteeMappingRepository.findAllByMentor(mentor)
+            .map(MentorMenteeMapping::mentee)
+            .toList()
+        return commentRepository.findAllByMenteeIn(mentee)
+            .map(GetCommentResponseDto::from)
+            .toList()
+    }
+
+    fun getAllSubComments(commentId: Long): List<GetSubCommentResponseDto> {
+        val comment = commentRepository.findByIdOrNull(commentId) ?: throw ServiceException(ErrorCode.NOT_FOUND)
+        return subCommentRepository.findAllByComment(comment)
+            .map(GetSubCommentResponseDto::from)
+            .toList()
     }
 
 }
