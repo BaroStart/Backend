dfed9cc0a79f7036189f6d915c8ce7f60e55bc94
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2026-02-07 21:56:33 +0900

    feat: 최대 연속 접속 날짜 조회 기능 구현

---
+++ src/main/kotlin/com/barostartbe/domain/user/usecase/AccessLogQueryUseCase.kt
@@ -0,0 +1,11 @@
+package com.barostartbe.domain.user.usecase
+
+import com.barostartbe.domain.user.repository.AccessLogRepository
+import com.barostartbe.global.annotation.QueryUseCase
+
+@QueryUseCase
+class AccessLogQueryUseCase(
+    private val accessLogRepository: AccessLogRepository
+) {
+    fun getMaxConsecutiveDays(menteeId: Long): Int = accessLogRepository.findMaxConsecutiveDays(menteeId)
+}
