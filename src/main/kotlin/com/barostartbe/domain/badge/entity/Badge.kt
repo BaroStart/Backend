4b7b6dc53f923f76961c5932f6a1786f7ae6f87e
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2026-02-08 00:06:09 +0900

    refactor: 오탈자 수정

--- src/main/kotlin/com/barostartbe/domain/badge/entity/Badge.kt
+++ src/main/kotlin/com/barostartbe/domain/badge/entity/Badge.kt
@@ -6,10 +6,10 @@ import jakarta.persistence.Entity
 import jakarta.persistence.Table
 
 @Entity
-@Table(name = "badeges")
+@Table(name = "badges")
 class Badge(
 
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, length = 50, unique = true)
     val name: String,
     ) : BaseEntity() {
 }
