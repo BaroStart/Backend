49f5ee41518badf554b6ded863d49f9deab5efd0
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2026-02-07 23:44:50 +0900

    feat: 뱃지 획득 기능 추가

--- src/main/kotlin/com/barostartbe/domain/badge/repository/MenteeBadgeMappingRepository.kt
+++ src/main/kotlin/com/barostartbe/domain/badge/repository/MenteeBadgeMappingRepository.kt
@@ -4,4 +4,6 @@ import com.barostartbe.domain.badge.entity.MenteeBadgeMapping
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface MenteeBadgeMappingRepository : JpaRepository<MenteeBadgeMapping, Long> {
+
+    fun findAllByMentee_id(menteeId: Long): List<MenteeBadgeMapping>
 }
