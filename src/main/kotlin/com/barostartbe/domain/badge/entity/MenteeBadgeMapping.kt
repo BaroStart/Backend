879f02a621a9458d9ac64a1e592f56e0712096cf
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2026-02-07 21:24:31 +0900

    feat: 뱃지 엔티티 생성

---
+++ src/main/kotlin/com/barostartbe/domain/badge/entity/MenteeBadgeMapping.kt
@@ -0,0 +1,19 @@
+package com.barostartbe.domain.badge.entity
+
+import com.barostartbe.domain.mentee.entity.Mentee
+import com.barostartbe.global.common.entity.BaseEntity
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "mentee_badge")
+class MenteeBadgeMapping(
+
+    @JoinColumn(name = "mentee_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    val mentee: Mentee,
+
+    @JoinColumn(name = "badge_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    val badge: Badge
+) : BaseEntity() {
+}
