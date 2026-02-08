e0bd758f8821d679eb004b0342f839afeabe7e82
hee_!J <heeej920@naver.com>
2026-02-08 14:35:57 +0900

    test: AssignmentGoal 제거에 따른 테스트 변경

--- src/test/kotlin/com/barostartbe/domain/assignmenttemplate/usecase/AssignmentTemplateByGoalQueryUseCaseTest.kt
+++ src/test/kotlin/com/barostartbe/domain/assignmenttemplate/usecase/AssignmentTemplateByGoalQueryUseCaseTest.kt
@@ -1,12 +1,12 @@
 package com.barostartbe.domain.assignmenttemplate.usecase
 
-import com.barostartbe.domain.assignment.entity.AssignmentGoal
 import com.barostartbe.domain.assignment.entity.enum.Subject
-import com.barostartbe.domain.assignment.repository.AssignmentGoalRepository
 import com.barostartbe.domain.assignmenttemplate.entity.AssignmentTemplate
 import com.barostartbe.domain.assignmenttemplate.entity.AssignmentTemplateFile
 import com.barostartbe.domain.assignmenttemplate.repository.AssignmentTemplateFileRepository
 import com.barostartbe.domain.assignmenttemplate.repository.AssignmentTemplateRepository
+import com.barostartbe.domain.mentor.entity.Mentor
+import com.barostartbe.domain.mentor.repository.MentorRepository
 import com.barostartbe.global.error.exception.ServiceException
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.assertions.throwables.shouldThrow
@@ -16,14 +16,14 @@ import java.util.*
 
 class AssignmentTemplateByGoalQueryUseCaseTest : DescribeSpec({
 
-    val assignmentGoalRepository = mockk<AssignmentGoalRepository>(relaxed = true)
     val assignmentTemplateRepository = mockk<AssignmentTemplateRepository>(relaxed = true)
     val assignmentTemplateFileRepository = mockk<AssignmentTemplateFileRepository>(relaxed = true)
+    val mentorRepository = mockk<MentorRepository>(relaxed = true)
 
     val useCase = AssignmentTemplateByGoalQueryUseCase(
-        assignmentGoalRepository,
         assignmentTemplateRepository,
-        assignmentTemplateFileRepository
+        assignmentTemplateFileRepository,
+        mentorRepository
     )
 
     beforeEach {
@@ -32,41 +32,46 @@ class AssignmentTemplateByGoalQueryUseCaseTest : DescribeSpec({
 
     describe("AssignmentTemplateByGoalQueryUseCase.execute") {
 
-        it("목표에 템플릿이 없는 경우 빈 리스트를 반환한다") {
+        it("목표명에 해당하는 템플릿이 없으면 빈 리스트를 반환한다") {
             // given
-            val goal = mockk<AssignmentGoal>(relaxed = true) {
+            val mentor = mockk<Mentor>(relaxed = true) {
                 every { id } returns 1L
-                every { name } returns "국어 독해"
-                every { subject } returns Subject.KOREAN
             }
 
             every {
-                assignmentGoalRepository.findById(1L)
-            } returns Optional.of(goal)
+                mentorRepository.findById(1L)
+            } returns Optional.of(mentor)
 
             every {
-                assignmentTemplateRepository.findAllByAssignmentGoalOrderByCreatedAtDesc(goal)
+                assignmentTemplateRepository
+                    .findAllByMentorAndSubjectAndNameOrderByCreatedAtDesc(
+                        mentor,
+                        Subject.KOREAN,
+                        "국어 독해"
+                    )
             } returns emptyList()
 
             // when
-            val result = useCase.execute(1L)
+            val result = useCase.execute(
+                mentorId = 1L,
+                subject = Subject.KOREAN,
+                goalName = "국어 독해"
+            )
 
             // then
-            result.goalId shouldBe 1L
+            result.goalName shouldBe "국어 독해"
             result.templates.size shouldBe 0
         }
 
-        it("목표에 템플릿과 파일이 존재하면 정상 조회된다") {
+        it("목표명에 해당하는 템플릿과 파일이 존재하면 정상 조회된다") {
             // given
-            val goal = mockk<AssignmentGoal>(relaxed = true) {
+            val mentor = mockk<Mentor>(relaxed = true) {
                 every { id } returns 1L
-                every { name } returns "국어 독해"
-                every { subject } returns Subject.KOREAN
             }
 
             val template = mockk<AssignmentTemplate>(relaxed = true) {
                 every { id } returns 10L
-                every { name } returns "독해 템플릿"
+                every { name } returns "국어 독해"
                 every { description } returns "설명"
                 every { title } returns "제목"
                 every { content } returns "내용"
@@ -80,11 +85,16 @@ class AssignmentTemplateByGoalQueryUseCaseTest : DescribeSpec({
             }
 
             every {
-                assignmentGoalRepository.findById(1L)
-            } returns Optional.of(goal)
+                mentorRepository.findById(1L)
+            } returns Optional.of(mentor)
 
             every {
-                assignmentTemplateRepository.findAllByAssignmentGoalOrderByCreatedAtDesc(goal)
+                assignmentTemplateRepository
+                    .findAllByMentorAndSubjectAndNameOrderByCreatedAtDesc(
+                        mentor,
+                        Subject.KOREAN,
+                        "국어 독해"
+                    )
             } returns listOf(template)
 
             every {
@@ -92,23 +102,30 @@ class AssignmentTemplateByGoalQueryUseCaseTest : DescribeSpec({
             } returns listOf(file)
 
             // when
-            val result = useCase.execute(1L)
+            val result = useCase.execute(
+                mentorId = 1L,
+                subject = Subject.KOREAN,
+                goalName = "국어 독해"
+            )
 
             // then
-            result.goalId shouldBe 1L
             result.templates.size shouldBe 1
             result.templates.first().files.size shouldBe 1
         }
 
-        it("목표가 존재하지 않으면 예외를 던진다") {
+        it("멘토가 존재하지 않으면 예외를 던진다") {
             // given
             every {
-                assignmentGoalRepository.findById(any())
+                mentorRepository.findById(any())
             } returns Optional.empty()
 
             // when / then
             shouldThrow<ServiceException> {
-                useCase.execute(999L)
+                useCase.execute(
+                    mentorId = 999L,
+                    subject = Subject.KOREAN,
+                    goalName = "국어 독해"
+                )
             }
         }
     }
