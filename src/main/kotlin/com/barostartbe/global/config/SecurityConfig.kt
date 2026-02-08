6e9793e7d0a862432ebae130b1e3ff28795202e4
hee_!J <heeej920@naver.com>
2026-02-08 18:41:46 +0900

    feat: 피드백 템플릿 에러 코드 추가

--- src/main/kotlin/com/barostartbe/global/config/SecurityConfig.kt
+++ src/main/kotlin/com/barostartbe/global/config/SecurityConfig.kt
@@ -56,7 +56,8 @@ class SecurityConfig(
             // 임시
             "/api/v1/assignments/**",
             "/api/v1/storages/**",
-            "/api/v1/files/**"
+            "/api/v1/files/**",
+            "/api/v1/feedback-templates/**"
 
 
         )
