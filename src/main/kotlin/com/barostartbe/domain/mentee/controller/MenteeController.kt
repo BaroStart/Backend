709d4df8cdec4a74910c395468fb35c532d753e8
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2026-02-09 01:13:52 +0900

    feat: 멘티 타임테이블 조회 기능 구현

--- src/main/kotlin/com/barostartbe/domain/mentee/controller/MenteeController.kt
+++ src/main/kotlin/com/barostartbe/domain/mentee/controller/MenteeController.kt
@@ -1,6 +1,7 @@
 package com.barostartbe.domain.mentee.controller
 
 import com.barostartbe.domain.mentee.dto.GetMenteeInfoResponseDto
+import com.barostartbe.domain.mentee.dto.TaskInfoResponseDto
 import com.barostartbe.domain.mentee.usecase.MenteeQueryUseCase
 import com.barostartbe.domain.user.entity.User
 import com.barostartbe.global.response.ApiResponse
@@ -9,13 +10,27 @@ import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
 
 @RestController
 class MenteeController(
-    val  menteeQueryUseCase: MenteeQueryUseCase
-): MenteeApi{
+    val menteeQueryUseCase: MenteeQueryUseCase
+) : MenteeApi {
 
-    override fun getMenteeInfo(@PathVariable menteeId: Long, @AuthenticationPrincipal mentor: User): ResponseEntity<ApiResponse<GetMenteeInfoResponseDto>> {
+    override fun getMenteeInfo(
+        @PathVariable menteeId: Long,
+        @AuthenticationPrincipal mentor: User
+    ): ResponseEntity<ApiResponse<GetMenteeInfoResponseDto>> {
         return ApiResponse.success(SuccessCode.REQUEST_OK, menteeQueryUseCase.getMenteeInfo(mentor.id!!, menteeId))
+    }
+
+    override fun getTimeTable(
+        @AuthenticationPrincipal mentee: User,
+        date: LocalDate?
+    ): ResponseEntity<ApiResponse<List<TaskInfoResponseDto>>> {
+        return ApiResponse.success(
+            SuccessCode.REQUEST_OK,
+            menteeQueryUseCase.getTimeTable(mentee.id!!, date ?: LocalDate.now())
+        )
     }
 }
--- src/main/kotlin/com/barostartbe/domain/mentee/usecase/MenteeQueryUseCase.kt
+++ src/main/kotlin/com/barostartbe/domain/mentee/usecase/MenteeQueryUseCase.kt
@@ -1,17 +1,23 @@
 package com.barostartbe.domain.mentee.usecase
 
 import com.barostartbe.domain.admin.repository.MentorMenteeMappingRepository
+import com.barostartbe.domain.assignment.entity.enums.AssignmentStatus
+import com.barostartbe.domain.assignment.repository.AssignmentRepository
 import com.barostartbe.domain.mentee.dto.GetMenteeInfoResponseDto
+import com.barostartbe.domain.mentee.dto.TaskInfoResponseDto
 import com.barostartbe.domain.mentee.entity.Mentee
 import com.barostartbe.domain.mentee.repository.MenteeRepository
 import com.barostartbe.domain.mentor.entity.Mentor
 import com.barostartbe.domain.mentor.repository.MentorRepository
+import com.barostartbe.domain.todo.entity.enums.Status
+import com.barostartbe.domain.todo.repository.ToDoRepository
 import com.barostartbe.domain.user.repository.AccessLogRepository
 import com.barostartbe.global.annotation.QueryUseCase
 import com.barostartbe.global.error.exception.ServiceException
 import com.barostartbe.global.response.type.ErrorCode
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.repository.findByIdOrNull
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
@@ -22,7 +28,9 @@ class MenteeQueryUseCase(
     val menteeRepository: MenteeRepository,
     val accessLogRepository: AccessLogRepository,
     val mentorMenteeMappingRepository: MentorMenteeMappingRepository,
-    val redisTemplate: RedisTemplate<String, Any>
+    val redisTemplate: RedisTemplate<String, Any>,
+    val assignmentRepository: AssignmentRepository,
+    val toDoRepository: ToDoRepository
 ) {
     fun getMenteeInfo(mentorId: Long, menteeId: Long): GetMenteeInfoResponseDto {
 
@@ -67,5 +75,17 @@ class MenteeQueryUseCase(
         val mapping = mentorMenteeMappingRepository.findByMentorAndMentee(mentor, mentee)
             ?: throw ServiceException(ErrorCode.UNMATCHED_PAIR)
         return DateTimeFormatter.ofPattern("yyyy-MM-dd").format(mapping.createdAt)
+    }
+
+    fun getTimeTable(menteeId: Long, date : LocalDate): List<TaskInfoResponseDto> {
+        val assignments = assignmentRepository.findAllByMentee_Id(menteeId)
+            .filter { it.status != AssignmentStatus.NOT_SUBMIT && it.startTime?.toLocalDate() == date }
+            .map { TaskInfoResponseDto.createAssignment(it) }
+
+        val todos = toDoRepository.findAllByMentee_IdAndStatus(menteeId, Status.COMPLETED)
+            .filter { it.startTime?.toLocalDate() == date }
+            .map { TaskInfoResponseDto.createToDo(it) }
+
+        return (assignments + todos).sortedBy { it.startTime }
     }
 }
