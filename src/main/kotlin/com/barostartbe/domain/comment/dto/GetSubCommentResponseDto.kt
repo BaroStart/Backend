de65463db5254951b8a1d515ec67fd59f53eb339
JaeYunChung <wjdwodbs2334@gmail.com>
2026-02-08 16:36:08 +0900

    fix: subCommentId 조회가능하도록 수정

--- src/main/kotlin/com/barostartbe/domain/comment/dto/GetSubCommentResponseDto.kt
+++ src/main/kotlin/com/barostartbe/domain/comment/dto/GetSubCommentResponseDto.kt
@@ -3,6 +3,7 @@ package com.barostartbe.domain.comment.dto
 import com.barostartbe.domain.comment.entity.SubComment
 
 data class GetSubCommentResponseDto(
+    val subCommentId: Long,
     val name: String,
     val subContent: String,
     val userType: String
@@ -10,6 +11,7 @@ data class GetSubCommentResponseDto(
     companion object{
         fun from(subComment: SubComment): GetSubCommentResponseDto{
             return GetSubCommentResponseDto(
+                subCommentId = subComment.id!!,
                 name = subComment.user.name!!,
                 subContent = subComment.content!!,
                 userType = subComment.user.role!!.name
