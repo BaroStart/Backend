ec0bbb8aac72e796d6494e8d71479e3780350c70
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2026-02-08 18:55:21 +0900

    feat: 알림 템플릿 속성 추가

--- src/main/kotlin/com/barostartbe/domain/notification/entity/enums/Type.kt
+++ src/main/kotlin/com/barostartbe/domain/notification/entity/enums/Type.kt
@@ -1,21 +1,23 @@
 package com.barostartbe.domain.notification.entity.enums
 
 enum class Type(
-    val description: String
+    val description: String,
+    val titleFormat: String,
+    val messageFormat: String
 ) {
     // mentor
-    ASSIGNMENT_SUBMITTED("과제 제출"),
-    FEEDBACK_REQUIRED("피드백 요청"),
+    ASSIGNMENT_SUBMITTED("과제 제출", "과제 제출 알림", "%s 멘티님께서, [%s - %s] 과제를 제출하였습니다."),
+    FEEDBACK_REQUIRED("피드백 요청", "피드백 요청 알림", "%s 멘티님께서, [%s - %s] 과제를 제출하였습니다."),
 
     // mentee
-    FEEDBACK_RECEIVED("피드백 수신"),
-    NEW_ASSIGNMENT("새 과제 배정"),
-    UNSUBMIT_ASSIGNMENT("마감된 과제 미제출"),
-    DEADLINE_ASSIGNMENT("과제 마감 임박"),
-    NEW_BADGE("새 뱃지 획득"),
-    NEW_COMMENT("새로운 코멘트"),
+    FEEDBACK_RECEIVED("피드백 수신", "새 피드백 도착", "%s 멘토님께서, [%s - %s] 과제에 피드백을 남겼습니다."),
+    NEW_ASSIGNMENT("새 과제 배정", "새 과제 도착", "%s 멘토님께서, [%s - %s] 과제를 새로 배정했습니다."),
+    UNSUBMIT_ASSIGNMENT("마감된 과제 미제출", "미완료 과제 알림", "어제, 마감된 [%s - %s] 과제가 제출되지 않았습니다."),
+    DEADLINE_ASSIGNMENT("과제 마감 임박", "과제 마감 임박!", "%s 과제의 마감 기한까지 %s분 남았습니다."),
+    NEW_BADGE("새 뱃지 획득", "새 뱃지 획득!", "%s 뱃지를 획득하였습니다."),
+    NEW_COMMENT("새로운 코멘트", "새로운 코멘트 도착", "%s님이 새로운 코멘트를 남겼습니다."),
 
     // common
-    NOTICE("공지사항"),
-    ETC("기타")
+    NOTICE("공지사항","%s", "%s"),
+    ETC("기타", "%s", "%s")
 }
