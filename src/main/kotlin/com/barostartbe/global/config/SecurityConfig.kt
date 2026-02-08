4b892842041fbd98806f6e191737ba771a27f8ad
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2026-02-08 18:25:33 +0900

    refactor: 빠른 테스트를 위한 API 요청 허용

--- src/main/kotlin/com/barostartbe/global/config/SecurityConfig.kt
+++ src/main/kotlin/com/barostartbe/global/config/SecurityConfig.kt
@@ -59,8 +59,6 @@ class SecurityConfig(
             "/api/v1/assignments/**",
             "/api/v1/storages/**",
             "/api/v1/files/**"
-
-
         )
 
         http {
@@ -88,7 +86,8 @@ class SecurityConfig(
                 authorize ("/admin/**", hasRole("ADMIN"))
 
                 // default
-                authorize(anyRequest, authenticated)
+//                authorize(anyRequest, authenticated)
+                authorize(anyRequest, permitAll)
             }
         }
 
