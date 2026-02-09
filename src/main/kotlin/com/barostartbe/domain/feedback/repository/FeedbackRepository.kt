06c8b9f3d1661f9fa9a38bfda848ff5ff49e56b9
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2026-02-09 21:51:58 +0900

    fix: 의존성 추가

--- src/main/kotlin/com/barostartbe/domain/feedback/repository/FeedbackRepository.kt
+++ src/main/kotlin/com/barostartbe/domain/feedback/repository/FeedbackRepository.kt
@@ -5,6 +5,7 @@ import com.barostartbe.domain.feedback.entity.Feedback
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.Optional
 
