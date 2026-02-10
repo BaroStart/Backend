8b38de7ad3f51969d919e31c5ef3af7c294bfe68
JaeYunChung <wjdwodbs2334@gmail.com>
2026-02-10 10:41:52 +0900

    fix: comment_id 응답에 추가

--- src/main/kotlin/com/barostartbe/domain/comment/dto/GetCommentResponseDto.kt
+++ src/main/kotlin/com/barostartbe/domain/comment/dto/GetCommentResponseDto.kt
@@ -4,6 +4,7 @@ import com.barostartbe.domain.comment.entity.Comment
 import java.time.LocalDateTime
 
 data class GetCommentResponseDto (
+    val id: Long,
     val content: String,
     val name: String,
     val createdAt: LocalDateTime,
@@ -11,6 +12,7 @@ data class GetCommentResponseDto (
     companion object{
         fun from(comment: Comment): GetCommentResponseDto{
             return GetCommentResponseDto(
+                id = comment.id!!,
                 content = comment.content!!,
                 name = comment.mentee!!.name!!,
                 createdAt = comment.createdAt!!,
